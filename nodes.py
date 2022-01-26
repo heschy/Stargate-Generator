f79f7d913dc607222120937690b7c1d361392e47
Heschy-Software <82758794+heschy@users.noreply.github.com>
2022-01-26 19:54:14 +0100

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -1,1 +1,11 @@
 import bpy
+
+# This Dictionarys are used to find Nodes easily, instead of using the NodeID (e.g: 'MixRGB' instad of 'ShaderNodeMixRGB')
+nodenames = {
+	'join':      "GeometryNodeJoinGeometry",
+    'math':      'ShaderNodeMath',
+    'cylinder':  "GeometryNodeMeshCylinder",
+    'boolean':   'GeometryNodeMeshBoolean',
+    'material':  'GeometryNodeSetMaterial',
+    'transform': 'GeometryNodeTransform'
+}
